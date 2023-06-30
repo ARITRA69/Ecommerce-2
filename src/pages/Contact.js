af3a706a2ac87f501e1684692b7bdcd785e2a4be
swarnadeep <aritrasarkar2002@gmail.com>
2023-06-30 13:23:11 +0530

    hi

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,15 +3,16 @@ import contactbg from "../img/contactbg.jpg";
 
 const Contact = () => {
   return (
-    <div
-      className="h-screen"
-      style={{
-        contactbg: `url(${contactbg})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundAttachment: "fixed",
-      }}
-    ></div>
+    <div className="h-screen bg-red-300 grid grid-cols-1 sm:grid-cols-2">
+      <div className="h-full relative">
+        <img src={contactbg} className="w-full h-full object-cover" />
+        <div class="absolute inset-0 bg-black opacity-70"></div>
+        <div class="absolute inset-0 flex items-center justify-center">
+          <h2 class="text-white text-4xl font-bold">Contact Us</h2>
+        </div>
+      </div>
+      <div className="bg-green-300 h-full"></div>
+    </div>
   );
 };
 
